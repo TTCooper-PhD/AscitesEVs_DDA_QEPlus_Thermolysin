200b5568f9904163c490f6d24b1b356639e92aa5
TTCooper-PhD <tcoope2@gmail.com>
2023-01-03 13:35:41 -0500

    Update MQ Folder

--- QE_DDA_bRP_AscitesEVs_Thermolysin.py
+++ QE_DDA_bRP_AscitesEVs_Thermolysin.py
@@ -2,15 +2,32 @@ import os
 import re
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib as plot
 
 
+colours={"TrypLysC":"green","Thermolysin":"purple"}
+colour_scheme="PrGn"
+dpi=300
 
+
+### MAX QUANT ANALYSES
 #MQ Files
 os.listdir("MQ")
 os.listdir("MQ/Grouped")
 os.listdir("MQ/Seperated")
+p2_MQ_proteins=os.path.join(os.getcwd, "MQ","Seperated","proteinGroups.txt")
+p2_MQ_proteins
+df = pd.read_csv("C:\Users\tcoop\Desktop\AscitesEVs_DDA_QEPlus_Thermolysin\MQ_\Seperated\proteinGroups.txt", delimiter = "\t")
+print(df)
+
+
+## PEAK ANALYSES
 #PEAKS Files
 os.listdir("PEAKS")
+
+
+## FragPipe Analyses
 #FragPipe Files
 os.listdir("FragPipe")
 
