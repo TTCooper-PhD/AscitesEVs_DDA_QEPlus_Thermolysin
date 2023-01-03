ff15d4fdc919aa6858320c0e03739c4e28c44515
TTCooper-PhD <tcoope2@gmail.com>
2023-01-03 18:10:57 -0500

    Updated Analysis

--- QE_DDA_bRP_AscitesEVs_Thermolysin.py
+++ QE_DDA_bRP_AscitesEVs_Thermolysin.py
@@ -1,41 +1,22 @@
-#pd.options.display.max_rows = None
-from itertools import combinations
+import os 
 import re
-from Peptide_Chef import Bakery 
-
-from pyteomics import parser, electrochem
-import math
 import pandas as pd
 import numpy as np
 
-#Statistics
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-
-#Figure Generation
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import matplotlib.colors as colors
-from matplotlib import offsetbox
-from matplotlib.offsetbox import AnchoredText
-from matplotlib.ticker import NullFormatter
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.tri import Triangulation
-from mpl_toolkits.mplot3d import axes3d
-from IPython.display import Image, display
-import seaborn as sns
-from adjustText import adjust_text
-import glob
-
-#Venn Diagrams
-from matplotlib_venn import venn2, venn2_circles, venn2_unweighted
-from matplotlib_venn import venn3, venn3_circles
 
 
-cmap = 'PRGn'
-fmt='eps'
-dpi=600
+#MQ Files
+os.listdir("MQ")
+os.listdir("MQ/Grouped")
+os.listdir("MQ/Seperated")
+#PEAKS Files
+os.listdir("PEAKS")
+#FragPipe Files
+os.listdir("FragPipe")
+
+#Updated
+
+
 
 
 
